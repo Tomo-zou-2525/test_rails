d5efb077511b9c740ee4bddd9efbacd7f3cc0832
友造-2525 <bathbook1990tomozou@gmail.com>
2020-09-05 18:00:01 +0900

    feat:コメント保存処理の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,28 @@
 class CommentsController < ApplicationController
   def create
+    # comment.newの中にboard_idが入っているので、関連付けはOK
+    comment = Comment.new(comment_params)
+    if comment.save
+      flash[:notice] = "『コメントを投稿しました』"
+      # コメントに紐づく掲示板の詳細画面に戻す
+      # このcomment.boardの記述が適用されるのは、belongs_toがあるから
+      redirect_to comment.board
+    else
+     flash[:comment] = comment
+		 flash[:error_messages] = comment.errors.full_messages
+		 redirect_back fallback_location: comment_board
+			# redirect_to :back, flash: {
+			# 	comment: comment,
+			# 	error_messages: comment.errors.full_messages
+			# }
+			end
   end
 
   def destroy
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:name, :comment, :board_id)
   end
 end
